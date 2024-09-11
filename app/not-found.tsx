cc0f64f5777be892f6c92e61f6cdb75310324b2c
abhay-01 <pratapsinghabhay999@gmail.com>
2024-09-11 15:43:27 +0530

    characters

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,8 +5,8 @@ export default function NotFound() {
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-800">404 - Page Not Found</h1>
-        <p className="text-gray-600 mt-4">Oops! Looks like you encountered a page that doesn&#39;t exist.</p>
-        <p className="text-gray-600 mt-2">You can go back to <Link href="/" className='text-red-400 font-bold'>home</Link> or try searching for what you're looking for.</p>
+        <p className="text-gray-600 mt-4">Page is not existed.</p>
+        <p className="text-gray-600 mt-2">You can go back to <Link href="/" className='text-red-400 font-bold'>home</Link> or try searching for other things.</p>
       </div>
     </div>
   );
